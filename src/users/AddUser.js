4adef981d6bdd4536daf9a5fc56ad5c8f883ad2f
felipe neves <felipe.ferreira@saamauditoria.com.br>
2023-10-04 20:48:06 -0300

    `feat(api)Capturando  os campos de texto`: adicionando a captura dos valores dos campos de texto.

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AddUser() {
-  return <div className="container">
+  const [user, setUser] = useState({
+    name:"",
+    username:"",
+    email:""
+  });
+
+  const {name, username, email} = user;
+
+  const onInputChange=(e)=>{
+    setUser({...user,[e.target.name]:e.target.value})
+  }
+
+  return (<div className="container">
     <div className="row">
       <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
         <h2 className="text-center m-4"> Registro de Usuário</h2>
@@ -12,6 +24,8 @@ export default function AddUser() {
             className ="form-control"
             placeholder="Digite seu nome"
             name='name'
+            value={name}
+            onChange={(e)=>onInputChange(e)}
           />
         </div>
         <div className="mb-3">
@@ -21,6 +35,8 @@ export default function AddUser() {
             className ="form-control"
             placeholder="Digite seu Login"
             name='username'
+            value={username}
+            onChange={(e)=>onInputChange(e)}
           />
         </div>
         <div className="mb-3">
@@ -30,12 +46,14 @@ export default function AddUser() {
             className ="form-control"
             placeholder="Digite seu email"
             name='email'
+            value={email}
+            onChange={(e)=>onInputChange(e)}
           />
         </div>
         <button type="submit" className="btn btn-outline-primary">Salvar</button>
         <button type="submit" className="btn btn-outline-danger mx-2">Cancelar</button>
       </div>
     </div>
-  </div>;
+  </div>);
 
 }
